46555a8748a011d8c73ebfa6066463b3cb3c3c9a
Dzzjio <giojio14@gmail.com>
2024-04-14 23:05:19 +0400

    add projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+import styles from "@/app/page.module.css";
+
+const ProjectsPage = () => {
+    return (
+        <div className={styles.main}>Projects pageaa</div>
+    )
+}
+
+export default ProjectsPage
