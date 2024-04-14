4d974a7f1d21d9b89e61df1e071d151418d4f306
Dzzjio <giojio14@gmail.com>
2024-04-14 22:22:03 +0400

    add constants

---
+++ constants.ts
@@ -0,0 +1,17 @@
+export const Colors = {
+    brandColor: 'rgb(34, 140, 34)',
+    brandColorTransparent: 'rgba(34, 140, 34, .8)'
+} 
+
+export const Transition = {
+    veryFast: 'transition: all .1s ease-in-out',
+    fast: 'transition: all .3s ease-in-out',
+    slow: 'transition: all 1s ease-in-out'
+}   
+
+export const Breakpoints = {
+    tabletland: '1024px',
+    tablet: '744px',
+    mobile: '480px',
+}
+
