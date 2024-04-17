d6ac74c92bb3846d27a53fdfce648f80388d8bb5
Dzzjio <giojio14@gmail.com>
2024-04-17 17:57:01 +0400

    add home page animations

--- components/partials/homeSection2.tsx
+++ components/partials/homeSection2.tsx
@@ -16,13 +16,13 @@ const HomeSectionTwo = () => {
         <section className='home-section-2'>
             <div className='home-section-2-animation' >
                 <Image className='home-scroll-img-1' alt='home-img-1' src={grayImg} data-aos='fade-left'/>
-                <div>
+                <div data-aos='fade-right'>
                     <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae hic saepe tenetur praesentium, optio incidunt, esse facere nulla odit vel accusamus ab.</h2>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates ratione, repellendus dignissimos enim velit autem quibusdam sed delectus nesciunt animi optio? Doloremque odio culpa quae nisi facere sunt consequuntur.</p>
                 </div>
             </div>
             <div className='home-section-2-animation' >
-                <div>
+                <div data-aos='fade-left'>
                     <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae hic saepe tenetur praesentium, optio incidunt, esse facere nulla odit vel accusamus ab.</h2>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates ratione, repellendus dignissimos enim velit autem quibusdam sed delectus nesciunt animi optio? Doloremque odio culpa quae nisi facere sunt consequuntur.</p>
                 </div>
